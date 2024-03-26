76fae8304b43588308cbbf8e68c445e71041b691
Bilal FAYE <57540322+b-faye@users.noreply.github.com>
2024-03-26 10:16:05 +0100

    version 1

--- tf-cluster-based-norm/tf_cluster_based_norm/SCBNorm.py
+++ tf-cluster-based-norm/tf_cluster_based_norm/SCBNorm.py
@@ -38,7 +38,7 @@ class SCBNorm(tf.keras.layers.Layer):
         )
         super(SCBNorm, self).build(input_shape)
         
-    def config(self):
+    def get_config(self):
         config = super().get_config().copy()
         config.update({
         	"num_clusters": self.num_clusters,
